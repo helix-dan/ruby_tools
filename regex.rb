4192a19852e70e25b10368c93fbfcbbe35aa8da1
helix-dan <lotus.bat@gmail.com>
2013-12-18 19:34:29 +0800

    add more regex

--- regex.rb
+++ regex.rb
@@ -3,8 +3,13 @@
 ## Email ##
 RUBULAR = /\A(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})\Z/
 
-## Phone ##
+## Phone for China ##
+/\A((\+86)?|\(\+86\))0?1[358]\d{9}\z/
 
 ## Software Version
+RUBULAR = /\d+(\.\d+)+/
 
-## ...
+## URL
+RUBULAR = /[a-zA-z]+:\/\/[^\s]*/
+
+## more ...
